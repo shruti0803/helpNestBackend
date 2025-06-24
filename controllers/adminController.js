81fba4e7a08e573940ad3579e5acdbe498cf52a5
Shruti Chaurasia <shchaurasia@adobe.com>
2025-06-24 11:28:04 +0530

    manage helper

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -297,3 +297,50 @@ export const getBookingsByCity = async (req, res) => {
   }
 };
 
+
+
+
+
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find(); // You can add filters here if needed
+    res.status(200).json(users);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).json({ message: 'Server Error' });
+  }
+};
+
+export const getAllHelpers = async (req, res) => {
+  try {
+    const users = await Helper.find(); // You can add filters here if needed
+    res.status(200).json(users);
+  } catch (error) {
+    console.error("Error fetching helpers:", error);
+    res.status(500).json({ message: 'Server Error' });
+  }
+};
+
+
+export const verifyHelper = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const updatedHelper = await Helper.findByIdAndUpdate(
+      id,
+      { isVerified: true },
+      { new: true }
+    );
+
+    if (!updatedHelper) {
+      return res.status(404).json({ message: 'Helper not found' });
+    }
+
+    res.status(200).json({ message: 'Helper verified', helper: updatedHelper });
+  } catch (error) {
+    console.error('Error verifying helper:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { adminLogin, adminLogout, getAdminProfile, getBookingsByCategory, getBookingsByCity, getDailyBookings, getEarningByMonth, getHelperSummary, getUserSummary } from "../controllers/adminController.js";
+import { adminLogin, adminLogout, getAdminProfile, getAllHelpers, getAllUsers, getBookingsByCategory, getBookingsByCity, getDailyBookings, getEarningByMonth, getHelperSummary, getUserSummary, verifyHelper } from "../controllers/adminController.js";
 import isAdminAuthenticated from "../middleware/isAdminMiddleware.js";
 
 
@@ -20,5 +20,7 @@ router.get('/users/summary', getUserSummary);
 router.get('/helpers/summary',getHelperSummary);
 router.get('/bookings-by-category', getBookingsByCategory);
 router.get('/bookings-by-city',getBookingsByCity);
-
+router.get("/allUsers", getAllUsers);
+router.get('/allHelpers', getAllHelpers);
+router.patch('/verify/:id', verifyHelper);
 export default router;
