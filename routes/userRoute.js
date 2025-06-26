3ffafc732915a9f6ccedfaab40cd9c326202f14d
Shruti Chaurasia <shchaurasia@adobe.com>
2025-06-26 21:09:36 +0530

    checked

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -8,14 +8,10 @@ const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', userLogin);
-router.get('/logout', Logout);
+router.get('/logout',isAuthenticated, Logout);
 
 router.get('/profile', isAuthenticated, getMyProfile);
 
 
-// router.get('/training/progress', isAuthenticated, getTrainingProgress);
-// router.post('/training/progress', isAuthenticated, updateTrainingProgress);
 
-// router.get('/test-score', isAuthenticated, getTestScore);
-// router.post('/test-score', isAuthenticated, updateTestScore);
 export default router;
